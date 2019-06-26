d64b2c62ddcb98c690d3339562c1369d814cd56c
0shimax <shin.keeper@gmail.com>
2019-06-26 15:01:58 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,12 @@ gcloud docker -- push gcr.io/${PROJECT_ID}/hello-flask-app:v2
 echo "Creating cluster"
 gcloud container clusters create hello-flask-cluster --num-nodes=1 --zone us-central1-b
 
-echo "Creating the flask deployment"
-kubectl apply -f ./kubernetes/flask-deployment.yml
+echo "Creating the flask deployment and service"
+kubectl create -f ./kubernetes/flask-deployment.yml
+kubectl create -f ./kubernetes/flask-service.yml
 
-kubectl run hello-flask-web --image=gcr.io/${PROJECT_ID}/hello-flask-app:latest --port 8080
+echo "Get external IP address"
+kubectl describe service flask
 
-kubectl expose deployment hello-flask-web --type=LoadBalancer --port 80 --target-port 8080
+# kubectl run hello-flask-web --image=gcr.io/${PROJECT_ID}/hello-flask-app:latest --port 8080
+# kubectl expose deployment hello-flask-web --type=LoadBalancer --port 80 --target-port 8080
