ea98a83b83c8311070f0eaeb313d48e21c8f825a
0shimax <shin.keeper@gmail.com>
2019-06-26 13:45:44 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,8 @@ echo "Creating cluster"
 gcloud container clusters create hello-flask-cluster --num-nodes=1
 
 echo "Creating the flask deployment"
-kubectl create -f ./kubernetes/flask-deployment.yml
+kubectl apply -f ./kubernetes/flask-deployment.yml
 
-kubectl run hello-flask-web --image=gcr.io/${PROJECT_ID}/hello-flask-app:v1 --port 5000
+kubectl run hello-flask-web --image=gcr.io/${PROJECT_ID}/hello-flask-app:v1 --port 8080
+
+kubectl expose deployment hello-flask-web --type=LoadBalancer --port 80 --target-port 8080
