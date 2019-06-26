3bbd081ee7ba63b842a60b1029af5efc44a1b030
0shimax <shin.keeper@gmail.com>
2019-06-26 10:42:39 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "Creating the database credentials..."
-kubectl apply -f ./kubernetes/secret.yml
+echo "Building and upload flask app image"
+docker build -t gcr.io/${PROJECT_ID}/hello-flask-app:v1 .
+gcloud docker -- push gcr.io/${PROJECT_ID}/hello-flask-app:v1
 
+echo "Creating cluster"
+gcloud container clusters create hello-flask-cluster --num-nodes=1
 
-echo "Creating the flask deployment and service..."
+echo "Creating the flask deployment"
 kubectl create -f ./kubernetes/flask-deployment.yml
-kubectl create -f ./kubernetes/flask-service.yml
 
-
-echo "Adding the ingress..."
-kubectl apply -f ./kubernetes/ingress.yml
+kubectl run hello-flask-web --image=gcr.io/${PROJECT_ID}/hello-flask-app:v1 --port 5000
